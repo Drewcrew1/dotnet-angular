a7d539faf065047edacd7b2f239f07c611babfa7
Drew <drewharper29@gmail.com>
2020-02-10 08:42:12 -0500

    set up routing with angular routing and protected routes

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from "@angular/forms";
@@ -10,6 +11,12 @@ import {AuthService} from "./_services/auth.service";
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import {ErrorInterceptorprovider} from "./_services/Error.interceptor";
+import {BsDropdownModule} from "ngx-bootstrap";
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {appRoutes} from "./routes";
+
 
 @NgModule({
   declarations: [
@@ -17,12 +24,18 @@ import {ErrorInterceptorprovider} from "./_services/Error.interceptor";
     ValueComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService, ErrorInterceptorprovider],
   bootstrap: [AppComponent]
--- DatingAppSPA/src/app/nav/nav.component.ts
+++ DatingAppSPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../_services/auth.service";
 import {AlertifyService} from "../_services/alertify.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,8 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   constructor(private authService: AuthService,
-              private alertify: AlertifyService) { }
+              private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,8 +21,11 @@ export class NavComponent implements OnInit {
   login(){
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Login Successful');
+
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -31,5 +36,6 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
